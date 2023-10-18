08b69b11a66e368c1cf63f13a0d1763ca4e98a27
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-10-18 22:31:14 +0530

    Add files via upload

---
+++ UNIT 4/LEC 28/Preethi is fascinated by trees, especially binary search trees.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    char data;
+    Node* left;
+    Node* right;
+};
+
+Node* insert(Node* root, char data) {
+    if (root == NULL) {
+        root = new Node();
+        root->data = data;
+        root->left = root->right = NULL;
+    } else if (data <= root->data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+Node* findMin(Node* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* deleteNode(Node* root, char data) {
+    if (root == NULL) {
+        return root;
+    }
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    } else {
+        // Node with only one child or no child
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // Node with two children, get the inorder successor (smallest in the right subtree)
+        Node* temp = findMin(root->right);
+
+        // Copy the inorder successor's content to this node
+        root->data = temp->data;
+
+        // Delete the inorder successor
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    int size;
+    cin >> size;
+    Node* root = NULL;
+    char input, char_to_delete;
+    while (size != 0) {
+        cin >> input;
+        root = insert(root, input);
+        size--;
+    }
+    cin >> char_to_delete;
+    root = deleteNode(root, char_to_delete);
+    inorder(root);
+    cout << endl;
+    return 0;
+}
+
