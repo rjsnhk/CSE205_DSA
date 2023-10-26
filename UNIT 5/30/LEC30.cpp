c7c8a3c92daaf72e31ee9eea177025e78ec0dbfb
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-10-26 23:18:28 +0530

    Add files via upload

---
+++ UNIT 5/30/LEC30.cpp
@@ -0,0 +1,179 @@
+//What is the maximum height of any AVL tree with 20 nodes?
+//
+//
+//
+//Note: Height of tree with single node is 0.
+//
+//Marks : 1
+//Negative Marks : 0
+//image
+//Answer here
+//5/
+//
+//
+//2
+//
+//
+//3
+//
+//
+//4
+
+The minimum height of an AVL tree with n nodes is 
+
+Marks : 1
+Negative Marks : 0
+image
+Answer here
+Ceil (log2 (n+1))
+
+
+1.44 log2 n
+
+
+//Floor (log2 (n+1))
+
+
+1.64 log2 n
+
+
+
+What maximum difference in heights between the leaves of an AVL tree is possible?
+
+Marks : 1
+Negative Marks : 0
+image
+Answer here
+log(n) where n is the number of nodes//
+
+
+n where n is the number of nodes
+
+
+0 or 1
+
+
+Atmost 1
+
+
+What is the balance factor of an AVL tree?
+
+
+
+Note: This kind of sample question is asked in the HCL interview.
+
+Marks : 1
+Negative Marks : 0
+image
+Answer here
+|h(TL) +h(TR)| = 1
+
+
+|h(TR)| = 1
+
+
+|h(TL)| = 1
+
+
+|h(TL) – h(TR)| <= 1//
+
+
+
+What is the worst-case time complexity of AVL tree insertion?
+
+
+
+Note: This kind of question will help in clearing Wipro recruitment.
+
+Marks : 1
+Negative Marks : 0
+image
+Answer here
+O(1)
+
+
+O(log n)//
+
+
+O(n)
+
+
+O(n log n)
+
+
+
+A node with a balance factor of 2 can be deleted by performing a single rotation.
+
+True
+
+
+//False
+
+In the AVL tree, which operation is NOT used during the deletion process?
+
+Rotation
+
+
+Insertion
+
+
+wrong-answer
+Traversal//
+
+
+wrong-answer
+Balancing
+
+
+In an AVL tree, when is rotation required during the deletion operation?
+
+Always
+
+
+When the tree is empty
+
+
+//When the balance factor of the node being deleted is -1 or +1 or 0
+
+
+wrong-answer
+Only for nodes with one child
+
+
+What is the time complexity of AVL tree deletion?
+
+
+
+Note: This kind of question is asked in the HCL requirement.
+
+O(log n)//
+
+
+wrong-answer
+O(n)
+
+
+O(n log n)
+
+
+It depends on the case and tree structure
+
+When deleting a node from an AVL tree, what is the maximum number of rotations required to restore balance, counting LR and RL rotations as a single rotation?
+
+
+
+Note: This kind of question will be helpful in clearing Wipro recruitment.
+
+1
+
+
+2//
+
+
+wrong-answer
+3
+
+
+4
+
+
