e3d1b3fbedbf5db3d256dc0dc05e2aa2b420bd1c
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-11-01 21:49:27 +0530

    Add files via upload

---
+++ UNIT 5/30/33/Krish  sort them in descending order using heap sort with max heap.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int n, int root) {
+    int largest = root;
+    int left = 2 * root + 1;
+    int right = 2 * root + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != root) {
+        swap(arr[root], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void buildMaxHeap(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+}
+
+void heapSort(int arr[], int n) {
+    buildMaxHeap(arr, n);
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    heapSort(arr, n);
+
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
